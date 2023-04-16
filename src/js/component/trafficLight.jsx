31b101abc99aa06ca48840a13673903852a7cd85
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-04-16 21:01:10 +0000

    Traffic Light with useState ready

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 const TrafficLight = () => {
+
+    const [colorSelected, setColorSelected] = useState("red");
+
+
     return(
         <>
+            <div className="card bg-dark mx-auto" style={{ width: "1rem", height: "10rem"}}>
+                
+            </div>
             <div className="card bg-dark mx-auto" style={{ width: "12rem"}}>
                 <div className="card-body d-flex flex-column align-items-center">
-                    <div className="red">
+                    <div onClick={() => setColorSelected("red")} className={`red ${colorSelected == "red" ? "brillo" : " "}`}>
                         red
                     </div>
-                    <div className="yellow">
+                    <div onClick={() => setColorSelected("yellow")} className={`yellow ${colorSelected == "yellow" ? "brillo" : " "}`}>
                         yellow
                     </div>
-                    <div className="green">
+                    <div onClick={() => setColorSelected("green")} className={`green ${colorSelected == "green" ? "brillo" : " "}`}>
                         green
                     </div>
                 </div>
