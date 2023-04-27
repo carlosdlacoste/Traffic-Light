5aa9f5462a2edbf855b6c2cabadabc8765959b4d
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-04-27 15:07:49 -0400

    Bono de boton para alternar color terminado

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -6,6 +6,12 @@ const TrafficLight = () => {
 
     const [colorSelected, setColorSelected] = useState("red");
 
+    const alternarColor = () => {
+        if(colorSelected == "red") setColorSelected("green")
+        if(colorSelected == "yellow")setColorSelected("red")
+        if(colorSelected == "green") setColorSelected("yellow")
+    }
+
 
     return(
         <>
@@ -24,6 +30,9 @@ const TrafficLight = () => {
                         green
                     </div>
                 </div>
+            </div>
+            <div className='d-flex justify-content-center mt-4'>
+                <button type="button" onClick={alternarColor} className="btn btn-secondary btn-lg btn-dark">Alternar Color</button>
             </div>
 
             
